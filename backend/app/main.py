5a540b97ba3e7924de7c2d090ffc54a573018e6c
Dany2-dev <up230203@alumnos.upa.edu.mx>
2026-01-27 17:14:57 -0600

    Encabezado4

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,7 @@ def startup():
 # ─────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.netlify\.",
+    allow_origin_regex=r"https://.*\.netlify\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
